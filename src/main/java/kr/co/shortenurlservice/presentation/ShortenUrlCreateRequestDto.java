a770a8c51971bba49bafa4b3669534849aa540c2
moguri846 <61900018+moguri846@users.noreply.github.com>
2024-09-12 20:24:00 +0900

    Update ShortenUrlCreateRequestDto.java

--- src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlCreateRequestDto.java
+++ src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlCreateRequestDto.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotNull;
 
 public class ShortenUrlCreateRequestDto {
     @NotNull
-    @URL(regexp = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
+    //@URL(regexp = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
     private String originalUrl;
 
     public ShortenUrlCreateRequestDto() {
